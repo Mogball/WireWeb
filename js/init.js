ec0d0cd2cb126a87cf5a7fb08a9b7ef3cc71c40e
Mogball <jeffniu22@gmail.com>
2017-02-08 15:24:03 -0500

    completed frontend login and register screens, started creating a script to automatically set the active navbar button, if avaiable

--- js/init.js
+++ js/init.js
@@ -1,5 +1,21 @@
-(function($){
-  $(function(){
-    $(".menu-button-collapse").sideNav();
-  });
+(function ($) {
+    $(function () {
+        // Enable the side navigation menu
+        $(".menu-button-collapse").sideNav();
+
+        // Set the active navigation button if possible
+        var navbar = document.getElementsByTagName("nav")[0];
+        // There should no more than one navigation bar
+        if (navbar) {
+            var selected_nav_btn = navbar.getAttribute("data-selected");
+            // One of: ["nav_features", "nav_services", "nav_company", "nav_career"]
+            if (selected_nav_btn) {
+                selected_nav_btn = document.getElementById(selected_nav_btn);
+                if (selected_nav_btn) {
+                    selected_nav_btn.setAttribute("class", "active"); // set active
+                }
+            }
+        }
+
+    });
 })(jQuery);
