7f1a791c82396ecd27e3bffa1b094b6ea4c16e15
Mogball <jeffniu22@gmail.com>
2017-02-28 12:17:23 -0500

    oops used focus event instead of change'

--- js/login.js
+++ js/login.js
@@ -144,7 +144,7 @@ $(function () {
             hasValue = $('#password:-webkit-autofill').length > 0;
         }
         if (hasValue) {
-            $password.trigger('focus');
+            $password.trigger('change');
         }
     }, 333);
 
